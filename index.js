36c61fbc7aa14457a68e7860130b930d9869cd32
Mike Papale <mpapale@splunk.com>
2015-06-05 15:04:19 -0700

    Make the server remember the last board state while its up

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var lastData = null;
+
 io.on('connection', function(socket) {
 	console.log('client connected');
 
+	if (lastData !== null) {
+		socket.emit('change:positions', lastData);
+	}
+
 	socket.on('change:positions', function(data) {
 		console.log('client changed position');
+		lastData = data;
 
 		socket.broadcast.emit('change:positions', data);
 	});
