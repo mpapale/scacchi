a0a634bf13b73561df7b51c76589f307dcadf2ae
Mike Papale <mpapale@splunk.com>
2015-06-06 23:38:39 -0700

    try to get sockets working with heroku

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
 
 var lastData = null;
 
 app.set('port', (process.env.PORT || 5000));
 app.use('/', express.static(__dirname + '/client'));
 
-io.on('connection', function(socket) {
+io.sockets.on('connection', function(socket) {
 	console.log('client connected');
 
 	if (lastData !== null) {
@@ -27,6 +27,6 @@ io.on('connection', function(socket) {
 	});
 });
 
-app.listen(app.get('port'), function() {
+server.listen(app.get('port'), function() {
 	console.log('listening on ', app.get('port'));
 });
