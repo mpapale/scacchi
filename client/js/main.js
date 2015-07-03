ad1253761381fef225f94746983232e1f4ae42ab
Mike Papale <mpapale@splunk.com>
2015-07-03 15:56:40 -0700

    hello-world example of react with scacchi

--- client/js/main.js
+++ client/js/main.js
@@ -5,12 +5,31 @@ require.config({
         'underscore': 'contrib/underscore/underscore-min',
         'backbone': 'contrib/backbone/backbone',
         'bootstrap': 'contrib/bootstrap/dist/js/bootstrap.min',
-        'socketio': 'contrib/socket.io-client/socket.io'
+        'socketio': 'contrib/socket.io-client/socket.io',
+        'react': 'contrib/react/react-with-addons',
+        'JSXTransformer': 'contrib/react/JSXTransformer',
+        'jsx': 'contrib/requirejs-react-jsx/jsx',
+        'text': 'contrib/requirejs-text/text'
     },
     shim: {
         'bootstrap': {
             deps: ['jquery'],
             exports: '$.fn.popover'
+        },
+        'react': {
+            exports: 'React'
+        },
+        'JSXTransformer': 'JSXTransformer'
+    },
+    config: {
+        jsx: {
+          fileExtension: '.jsx',
+          transformOptions: {
+            harmony: true,
+            stripTypes: false,
+            inlineSourceMap: true
+          },
+          usePragma: false
         }
     }
 });
@@ -22,7 +41,8 @@ require(
         'backbone',
         'bridges/Server',
         'models/Board',
-        'views/SvgBoard'
+        'views/SvgBoard',
+        'jsx!app'
     ], 
     function(
         $,
@@ -30,30 +50,33 @@ require(
         Backbone,
         ServerBridge,
         BoardModel,
-        SvgBoardView
+        SvgBoardView,
+        App
     ) {
-        var qsp = {
-            perspective: window.location.search.indexOf('black') !== -1 ? 'black' : 'white',
-            reset: window.location.search.indexOf('reset') !== -1 
-        };
-        var boardModel = new BoardModel();
-        var serverBridge = new ServerBridge({
-            board: boardModel
-        });
-        var boardView1 = new SvgBoardView({
-            className: 'float-left',
-            perspective: qsp.perspective,
-            model: {
-                board: boardModel
-            }
-        });
-        boardView1.render().$el.appendTo($('body'));
+        // var qsp = {
+        //     perspective: window.location.search.indexOf('black') !== -1 ? 'black' : 'white',
+        //     reset: window.location.search.indexOf('reset') !== -1 
+        // };
+        // var boardModel = new BoardModel();
+        // var serverBridge = new ServerBridge({
+        //     board: boardModel
+        // });
+        // var boardView1 = new SvgBoardView({
+        //     className: 'float-left',
+        //     perspective: qsp.perspective,
+        //     model: {
+        //         board: boardModel
+        //     }
+        // });
+        // boardView1.render().$el.appendTo($('body'));
 
-        // This doesn't work so great, needs a reload
-        // Should probably move the sync logic to the model
-        if (qsp.reset) {
-            boardModel.setDefault();
-            boardModel.save();
-        }
+        // // This doesn't work so great, needs a reload
+        // // Should probably move the sync logic to the model
+        // if (qsp.reset) {
+        //     boardModel.setDefault();
+        //     boardModel.save();
+        // }
+        var app = new App();
+        app.init();
     }
 );
