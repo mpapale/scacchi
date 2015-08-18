f5bd13376e3a65ee72d177888785c938d5c71efa
PJ Papale <pjpapale@gmail.com>
2015-08-18 11:50:01 -0400

    First Successful Read and Write to Postgesql

--- index.js
+++ index.js
@@ -3,13 +3,37 @@ var favicon = require('serve-favicon');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
-
 var lastData = null;
 
 app.set('port', (process.env.PORT || 5000));
 app.use(favicon(__dirname + '/client/images/favicon.ico'));
 app.use('/', express.static(__dirname + '/client'));
 
+
+var pg = require('pg');
+
+var conString = "postgres://postgres:wellcraft210dc@localhost/MyFirst";
+
+var dbclient = new pg.Client(conString);
+dbclient.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  dbclient.query('SELECT pet_name from ppapale.pet', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    } 	
+	
+	for (var i=0; i < result.rows.length; i++){
+		console.log(result.rows[i].pet_name);
+	}
+    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+	result.rows.forEach(function (row) {
+       console.log(row.pet_name);		
+	});
+//    dbclient.end();
+  });
+});
 
 io.sockets.on('connection', function(socket) {
 	var client = socket.client.request.connection.remoteAddress;
@@ -22,7 +46,16 @@ io.sockets.on('connection', function(socket) {
 	socket.on('change:positions', function(data) {
 		console.log(client, 'changed position');
 		lastData = data;
-
+		var command='insert into ppapale.scacchi values (\'' + JSON.stringify(data) + '\')';
+		console.log(command);
+        dbclient.query(command, function(err, result) {
+			if(err) {
+				console.error('error running query', err);
+			}
+			else {
+				console.log('successfully wrote to database')
+			}
+        });			
 		socket.broadcast.emit('change:positions', data);
 	});
 
